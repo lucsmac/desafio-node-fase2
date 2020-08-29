7d4cb740b9f67ffb53b83b5880fdfaf5ec221f7d
Lucas Macedo <lucasmdc57@gmail.com>
2020-08-29 18:06:04 -0300

    solved

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable class-methods-use-this */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const { transactions } = this;
+
+    return transactions;
+  }
+
+  private getSumByTransactionValue(type: string): number {
+    const sum = this.transactions
+      .filter(transaction => transaction.type === type)
+      .reduce((prev, curr) => {
+        return prev + curr.value;
+      }, 0);
+
+    return sum;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.getSumByTransactionValue('income');
+    const outcome = this.getSumByTransactionValue('outcome');
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
